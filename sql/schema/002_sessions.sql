5d7d30a7843097971c021f2ab0bacbe857b8c78e
Iyed-M <iyedmhiri01@gmail.com>
2024-04-09 01:27:41 +0100

    add auth modules

--- sql/schema/002_sessions.sql
+++ sql/schema/002_sessions.sql
@@ -1,5 +1,23 @@
 -- +goose Up
+CREATE TABLE PasswordLogin (
+  id uuid PRIMARY KEY,
+  user_id uuid REFERENCES Users(id),
+  email varchar(127),
+  password varchar(255),
+  last_login timestamp
+);
+
+CREATE TABLE Oauth (
+  id uuid PRIMARY KEY,
+  user_id uuid REFERENCES Users(id) ON DELETE CASCADE,
+  provider varchar(255),
+  avatar varchar(255),
+  email varchar(255),
+  username varchar(255)
+  );
+
 CREATE TYPE EPlatform AS ENUM ('neovim', 'web', 'desktop', 'mobile', 'cli','vscode');
+
 CREATE TABLE Sessions (
 id uuid PRIMARY KEY,
   user_id uuid REFERENCES Users(id),
@@ -7,8 +25,8 @@ id uuid PRIMARY KEY,
   refresh_token varchar(255),
   platform EPlatform,
   method varchar(255),
-  Oauth_id uuid, --fk
-  password_login_id uuid, --fk
+  Oauth_id uuid REFERENCES Oauth(id) ON DELETE CASCADE, --fk
+  password_login_id uuid REFERENCES password_login(id) ON DELETE CASCADE,
 
   created_at timestamp,
   last_login timestamp
@@ -16,3 +34,5 @@ id uuid PRIMARY KEY,
 
 -- +goose Down
 DROP TABLE Sessions;
+DROP TABLE Oauth;
+DROP TABLE PasswordLogin;
