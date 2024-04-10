9215f2dd2286ea7ff80b854a5148fe62ba0cf85e
Iyed-M <iyedmhiri01@gmail.com>
2024-04-10 09:54:00 +0100

    refactor auth sql schema

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,9 +3,10 @@ CREATE TABLE Users (
    id uuid PRIMARY KEY,
   user_name varchar(255) UNIQUE NOT NULL ,
   email varchar(255) UNIQUE NOT NULL,
+  password varchar(255) NOT NULL,
   created_at timestamp NOT NULL,
   last_login timestamp NOT NULL
 );
 
 -- +goose Down
-DROP TABLE Users ;
+DROP TABLE Users CASCADE ;
--- sql/schema/002_sessions.sql
+++ sql/schema/002_sessions.sql
@@ -1,41 +1,14 @@
 -- +goose Up
-CREATE TABLE PasswordLogin (
-  id uuid PRIMARY KEY,
-  user_id uuid NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
-  email varchar(127) UNIQUE NOT NULL,
-  password varchar(255) NOT NULL,
-  last_login timestamp NOT NULL
-);
-
-CREATE TABLE Oauth (
-  id uuid PRIMARY KEY,
-  user_id uuid NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
-  provider varchar(255) NOT NULL,
-  avatar varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  username varchar(255) NOT NULL
-  );
-
 CREATE TYPE EPlatform AS ENUM ('neovim', 'web', 'desktop', 'mobile', 'cli','vscode');
-
-CREATE TYPE EMethod AS ENUM ('password', 'oauth');
 CREATE TABLE Sessions (
    id uuid PRIMARY KEY,
   user_id uuid NOT NULL REFERENCES Users(id) ON DELETE CASCADE NOT NULL,
   access_token varchar(255)NOT NULL,
   refresh_token varchar(255) NOT NULL,
   platform EPlatform NOT NULL,
-
-  method EMethod NOT NULL,
-  Oauth_id uuid REFERENCES Oauth(id) ON DELETE CASCADE,
-  password_login_id uuid REFERENCES PasswordLogin(id) ON DELETE CASCADE,
   created_at timestamp NOT NULL,
   last_login timestamp NOT NULL
 );
-
 -- +goose Down
 DROP TABLE Sessions;
-DROP TABLE Oauth;
-DROP TABLE PasswordLogin;
 DROP TYPE IF EXISTS EPlatform;
-DROP TYPE IF EXISTS EMethod;
