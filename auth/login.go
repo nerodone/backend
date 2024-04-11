5b0afffa0d0fcfda940708e05b384dc5a64786fd
Iyed-M <iyedmhiri01@gmail.com>
2024-04-11 10:43:52 +0100

    login

--- auth/login.go
+++ auth/login.go
@@ -5,13 +5,22 @@ import (
 	"backend/server"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	ErrInvalidEmail    = "invalid email"
+	ErrInvalidPassword = "invalid password"
+	ErrInvalidRequest  = "Invalid request payload"
 )
 
 type loginReq struct {
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
+	Platform  string    `json:"platform"`
 	SessionId uuid.UUID `json:"session_id"`
 }
 
@@ -20,8 +29,15 @@ func (req *loginReq) validateRequest() bool {
 }
 
 type loginRes struct {
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token"`
+	ID           string    `json:"id"`
+	UserName     string    `json:"user_name"`
+	Email        string    `json:"email"`
+	CreatedAt    time.Time `json:"created_at"`
+	LastLogin    time.Time `json:"last_login"`
+	Platform     string    `json:"platform"`
+	SessionID    string    `json:"session_id"`
 }
 
 func login(s *server.Server) http.HandlerFunc {
@@ -29,25 +45,52 @@ func login(s *server.Server) http.HandlerFunc {
 		reqPayload := &loginReq{}
 
 		if err := json.NewDecoder(r.Body).Decode(reqPayload); err != nil || !reqPayload.validateRequest() {
-			s.RespondWithError(w, http.StatusBadRequest, "Invalid request payload", "err", err.Error())
+			s.RespondWithError(w, http.StatusBadRequest, ErrInvalidRequest, "err", err.Error())
 			return
 		}
 
-		hashedPassword, err := hashPassword(reqPayload.Password)
+		userDB, err := s.Db.LoginUser(s.Ctx, reqPayload.Email)
 		if err != nil {
-			s.RespondWithError(w, http.StatusBadRequest, "password too long", "err", err.Error())
+			s.RespondWithError(w, http.StatusUnauthorized, ErrInvalidEmail, "err", err.Error())
 			return
 		}
 
-		passwordID, err := s.Db.LoginUser(s.Ctx, database.LoginUserParams{
-			Password: hashedPassword,
-			Email:    reqPayload.Email,
+		if err := bcrypt.CompareHashAndPassword([]byte(userDB.Password), []byte(reqPayload.Password)); err != nil {
+			s.RespondWithError(w, http.StatusUnauthorized, ErrInvalidPassword, "err", err.Error())
+			return
+		}
+		refreshToken, err := s.JWT.EncodeToken(server.Payload{UserID: userDB.ID.String(), Username: userDB.UserName}, true)
+		if err != nil {
+			s.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error", "err_encoding_token", err.Error())
+			return
+		}
+		accessToken, err := s.JWT.EncodeToken(server.Payload{UserID: userDB.ID.String(), Username: userDB.UserName}, false)
+		if err != nil {
+			s.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error", "err_encoding_token", err.Error())
+			return
+		}
+		session, err := s.Db.CreateSessionWithPassword(s.Ctx, database.CreateSessionWithPasswordParams{
+			UserID:       userDB.ID,
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken,
+			Platform:     database.Eplatform(reqPayload.Platform),
 		})
 		if err != nil {
-			s.RespondWithError(w, http.StatusUnauthorized, "Invalid email or password", "err", err.Error())
+			s.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error", "db_err_creating_session", err.Error())
 			return
 		}
-		_ = passwordID
-		_ = loginRes{}
+
+		responsePayload := loginRes{
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken,
+			ID:           userDB.ID.String(),
+			SessionID:    session.ID.String(),
+			UserName:     userDB.UserName,
+			Email:        userDB.Email,
+			CreatedAt:    userDB.CreatedAt,
+			LastLogin:    userDB.LastLogin,
+			Platform:     reqPayload.Platform,
+		}
+		s.ResponsWithJson(w, http.StatusOK, responsePayload)
 	}
 }
