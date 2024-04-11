90e28de30270fba9b9e74b74b850d398cef977db
Iyed-M <iyedmhiri01@gmail.com>
2024-04-11 10:43:32 +0100

    refactor signup: more data on endpoint response

--- auth/signup.go
+++ auth/signup.go
@@ -4,7 +4,6 @@ import (
 	"backend/database"
 	"backend/server"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"time"
 )
@@ -21,12 +20,14 @@ func (req *UserSignupRequest) validateSignupRequest() bool {
 }
 
 type UserSignupResponse struct {
-	UserName     string `json:"user_name"`
-	Email        string `json:"email"`
-	LastLogin    string `json:"last_login"`
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-	SessionID    string `json:"session_id"`
+	ID           string    `json:"id"`
+	UserName     string    `json:"user_name"`
+	Email        string    `json:"email"`
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token"`
+	SessionID    string    `json:"session_id"`
+	LastLogin    time.Time `json:"last_login"`
+	CreatedAt    time.Time `json:"created_at"`
 }
 
 func signup(s *server.Server) http.HandlerFunc {
@@ -36,7 +37,6 @@ func signup(s *server.Server) http.HandlerFunc {
 			s.RespondWithError(w, http.StatusBadRequest, "Invalid request payload", "err", err.Error())
 			return
 		}
-		fmt.Println("1")
 
 		hashedPass, err := hashPassword(reqPayload.Password)
 		if err != nil {
@@ -44,7 +44,6 @@ func signup(s *server.Server) http.HandlerFunc {
 			return
 		}
 
-		fmt.Print("2")
 		user, err := s.Db.CreateUser(s.Ctx, database.CreateUserParams{
 			UserName: reqPayload.UserName,
 			Email:    reqPayload.Email,
@@ -55,22 +54,19 @@ func signup(s *server.Server) http.HandlerFunc {
 			return
 		}
 
-		fmt.Print("3")
 		accessToken, err := s.JWT.EncodeToken(server.Payload{UserID: user.ID.String(), Username: user.UserName}, false)
 		if err != nil {
 			s.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error", "err", err.Error())
 			return
 		}
 
-		fmt.Print("4")
 		refreshToken, err := s.JWT.EncodeToken(server.Payload{UserID: user.ID.String(), Username: user.UserName}, true)
 		if err != nil {
 			s.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error", "err", err.Error())
 			return
 		}
 
-		fmt.Print("5")
-		SessionID, err := s.Db.CreateSessionWithPassword(s.Ctx, database.CreateSessionWithPasswordParams{
+		Session, err := s.Db.CreateSessionWithPassword(s.Ctx, database.CreateSessionWithPasswordParams{
 			UserID:       user.ID,
 			Platform:     database.Eplatform(reqPayload.Platform),
 			AccessToken:  accessToken,
@@ -81,17 +77,15 @@ func signup(s *server.Server) http.HandlerFunc {
 			return
 		}
 		resp := &UserSignupResponse{
+			ID:           user.ID.String(),
 			UserName:     user.UserName,
 			Email:        user.Email,
-			LastLogin:    time.Now().Truncate(time.Second).UTC().String(),
+			LastLogin:    time.Now().Truncate(time.Second).UTC(),
 			AccessToken:  accessToken,
 			RefreshToken: refreshToken,
-			SessionID:    SessionID.String(),
+			SessionID:    Session.ID.String(),
+			CreatedAt:    Session.CreatedAt.Truncate(time.Second).UTC(),
 		}
-		err = s.ResponsWithJson(w, http.StatusCreated, resp)
-		if err != nil {
-			s.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error", "err", err.Error())
-			return
-		}
+		s.ResponsWithJson(w, http.StatusCreated, resp)
 	}
 }
