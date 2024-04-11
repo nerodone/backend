7807919e8649845ca1f78defe063a862e7d095f0
Iyed-M <iyedmhiri01@gmail.com>
2024-04-11 08:53:02 +0100

    refactor sql-queries : signup session returns *

--- sql/queries/sign_up_session.sql
+++ sql/queries/sign_up_session.sql
@@ -1,6 +1,6 @@
 -- name: CreateSessionWithPassword :one
 insert into Sessions (id , user_id , access_token , refresh_token , platform, created_at, last_login)
 values ( gen_random_uuid(), $1 , $2 , $3 , $4 , NOW(), NOW())
-returning id;
+returning *;
 
 
