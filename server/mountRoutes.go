49374d7351abac85f13707db5c70cc68c948dd33
Iyed-M <iyedmhiri01@gmail.com>
2024-04-14 07:37:53 +0100

    feat(auth) add authenticator option for protected routes

--- server/mountRoutes.go
+++ server/mountRoutes.go
@@ -2,15 +2,21 @@ package server
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
 )
 
 type Route struct {
-	Endpoint string
-	Handler  *chi.Mux
+	Endpoint     string
+	Handler      *chi.Mux
+	isAuthorized bool
 }
 
 func (s *Server) MountRoutes(route ...Route) {
 	for _, rt := range route {
-		s.App.Mount(rt.Endpoint, rt.Handler)
+		if rt.isAuthorized {
+			rt.Handler.Use(jwtauth.Verifier(s.JWT.TokenAuth))
+			rt.Handler.Use(jwtauth.Authenticator(s.JWT.TokenAuth))
+			s.App.Mount(rt.Endpoint, rt.Handler)
+		}
 	}
 }
