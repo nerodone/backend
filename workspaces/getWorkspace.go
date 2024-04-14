418b6a319b273c12efd41e38f667c0eb9357ecfb
Iyed-M <iyedmhiri01@gmail.com>
2024-04-14 18:09:12 +0100

    feat(workspace) add getWorkspaceByID endpoint

---
+++ workspaces/getWorkspace.go
@@ -0,0 +1,24 @@
+package workspaces
+
+import (
+	"backend/server"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+)
+
+func getWorkspaceByID(s *server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		workspace_id := chi.URLParam(r, "workspace_id")
+		id, err := uuid.Parse(workspace_id)
+		if err != nil {
+			s.RespondWithError(w, http.StatusInternalServerError, "internal server error", "err", err.Error())
+		}
+		workspace, err := s.Db.GetWorkspaceByID(r.Context(), id)
+		if err != nil {
+			s.RespondWithError(w, http.StatusInternalServerError, "internal server error", "err", err.Error())
+		}
+		s.RespondWithJson(w, http.StatusOK, workspace)
+	}
+}
--- workspaces/router.go
+++ workspaces/router.go
@@ -10,8 +10,7 @@ func workspaceRouter(s *server.Server) *chi.Mux {
 	r := chi.NewRouter()
 	r.Get("/", listWorkspaces(s))
 	r.Post("/", createWorkspace(s))
-	// TODO:
-	// r.Get("/{workspace_id}", getWorkspaceByID(s))
+	r.Get("/{workspace_id}", getWorkspaceByID(s))
 	// r.Put("/{workspace_id}", updateWorkspace(s)
 	// r.Delete("/{workspace_id}", deleteWorkspace(s)
 
