da0d6a48dbe077fb898a30e956e0d37f1b8c36fa
Iyed-M <iyedmhiri01@gmail.com>
2024-04-14 17:50:43 +0100

    feat(workspaces) refactor queries

--- sql/queries/workspaces.sql
+++ sql/queries/workspaces.sql
@@ -1,3 +1,8 @@
+-- name: CreateWorkspace :one
+insert into workspaces (id, name, owner, updated_at, created_at, description) values (
+  gen_random_uuid(),$1, $2, NOW(), NOW(), $3
+)
+returning *;
 
 -- name: GetAllWorkspaces :many
 --GetAllWorkspaces returns all workspaces that the user is the owner of or collaborates on.
@@ -10,3 +15,7 @@ OR EXISTS (
   WHERE c.user = sqlc.arg(user_id)::uuid
   AND c.workspace = w.id
 );
+
+-- name: GetWorkspaceByID :one
+SELECT * FROM workspaces WHERE id = $1;      
+
--- sql/queries/workspaces_create.sql
+++
@@ -1,5 +0,0 @@
--- name: CreateWorkspace :one
-insert into workspaces (id, name, owner, updated_at, created_at, description) values (
-  gen_random_uuid(),$1, $2, NOW(), NOW(), $3
-)
-returning *;
