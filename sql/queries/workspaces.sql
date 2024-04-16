c7f352153f2c471ddbb864d222c64f2c6d202dc8
Iyed-M <iyedmhiri01@gmail.com>
2024-04-16 16:10:50 +0100

    typo

--- sql/queries/workspaces.sql
+++ sql/queries/workspaces.sql
@@ -12,10 +12,19 @@ WHERE w.owner = sqlc.arg(user_id)::uuid
 OR EXISTS (
   SELECT 1
   FROM collaborate c
-  WHERE c.user = sqlc.arg(user_id)::uuid
-  AND c.workspace = w.id
+  WHERE c.user_id = sqlc.arg(user_id)::uuid
+  AND c.workspace_id = w.id
 );
 
 -- name: GetWorkspaceByID :one
 SELECT * FROM workspaces WHERE id = $1;      
+
+
+-- name: UpdateWorkspace :exec
+--UpdateWorkspace can only update the workspace name and description
+UPDATE workspaces SET name = $3, description = $2, updated_at = NOW() WHERE id = $1;
+
+
+-- name: DeleteWorkspace :exec
+DELETE FROM workspaces WHERE id = $1;
 
